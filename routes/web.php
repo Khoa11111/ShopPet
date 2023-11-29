cd12c20ffbfe4f7a8d0831189b7620d18ba79d15
Nguyen Van Khoa <khoanguyen04102002@gmail.com>
2023-11-29 20:39:53 +0700

    add project

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckOutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/product', [ProductController::class,'index']);
+Route::get('/cart', [CartController::class,'index']);
+Route::get('/checkout', [CheckOutController::class,'index']);
